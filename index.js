af58c048122d7aec4ea6020278dc9fc7bbfe15f5
Peter Tardif <peter.tardif@gmail.com>
2019-03-30 13:24:05 -0600

    initial commit of code, switching drivers

---
+++ index.js
@@ -0,0 +1,31 @@
+$(function(){
+    $('#js-shopping-list-form').on('click', 'button', function(event){
+        event.preventDefault();
+        let itemInput = $("#shopping-list-entry").val();
+        $("#shopping-list-entry").val('');
+        $('.shopping-list').append(
+            `<li><span class="shopping-item">${itemInput}</span>
+                <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+                </div>
+            </li>`)
+    })
+
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+        event.preventDefault();
+        console.log(event.target);
+        $(this).closest('li',function(){
+            $('.shopping-item').toggleClass('shopping-item__checked');
+        })
+    })
+
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event){
+        event.preventDefault();
+        
+    })
+})
